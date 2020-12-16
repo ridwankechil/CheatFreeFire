e112728e78f2cdbf35c00378e301b035fe85f162
ridwankechil <70096802+ridwankechil@users.noreply.github.com>
2020-12-16 17:06:48 +0700

    Create freefire.sh

---
+++ freefire.sh
@@ -0,0 +1,65 @@
+#!/system/xbin/bash
+#mau Recode izin dulu di Instagram Ridwan58aja
+#Github:https://github.com/RidwanKechil
+bi='\033[34;1m' #biru
+ij='\033[32;1m' #ijo
+pr='\033[35;1m' #purple
+cy='\033[36;1m' #cyan
+me='\033[31;1m' #merah
+pu='\033[37;1m' #putih
+ku='\033[33;1m' #kuning
+or='\033[1;38;5;208m' #Orange
+echo
+echo $ku "Selamat Datang Di Github Gw"
+echo $me "###########################"
+echo "##" $ku " github: github.com/RidwanKechil"
+echo $me "##" $ku "creator : Ridwan Kechil"
+echo $me "##" $ku "Subscribe : Ridwan Kechil "
+echo $me "###########################"
+echo $cy " Cheat Free Fire New"
+echo $me "jika membutuhkan script tentang freefire kunjungi whatsapp gw"
+termux-open https://wa. me/+6285731928045?text=Ada+Script+Apa+Saja+Kak+
+echo "Siapa Nama Kamu???... "
+read -p "root@[Ridwan•Kechil]=> " nick
+echo $ku "Selamat Datang " $nick
+echo
+clear
+
+sleep 3
+echo $me "Subscribe ya" $nick
+echo $ku "membuka Youtube"
+termux-open https://www.youtube.com/channel/UCTMBlJoMW9jaWyiv8oZsjKw
+echo $or "########################"
+echo "##" $ij "1. Mod Menu Free fire Lengkap"
+echo $ku "====>fitur<====="
+echo "=>mod menu lengkap"
+echo $or "##" $ij "2. Script Auto Heatshot"
+echo $ku "=>Berbentuk Script tingal di paste aja "
+echo $me "[!] Kalo Gak Faham Ada Tutorialnya nanti"
+echo $or "pilih nomor 1/2"
+read -p "[pilih 1/2]> " bro
+if [ $bro = 1 ] || [ $bro = 1 ]
+then
+echo
+echo
+clear
+
+echo "Sabar Broo"
+echo $ku "Langsung Download aja broo "
+echo $me "No Password"
+termux-open https://www.mediafire.com/download/0jz1wt2ko5h1zep
+fi
+fi
+
+if [ $bro = 2 ] || [ $bro = 2 ]
+then
+
+
+echo $ku "script Tambah Damage Free Fire"
+echo $bi "Password :SayankKamu"
+echo $ku "estrak pakek z Archirver"
+sleep 4
+termux-open https://www.mediafire.com/download/ihfaewe1rkknpvu
+fi
+fi
+echo " makasih" $pu
